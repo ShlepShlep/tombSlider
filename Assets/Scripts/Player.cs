4230fb9e59ca194c0bcf941040cbdfaaa7c9943e
ShlepShlep <agne.sileviciute@gmail.com>
2024-04-17 20:01:35 +0300

    zsfas

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 5;
+    public GameObject landParticles;
+    public float speed = 8;
     Rigidbody2D rb;
     Vector2 input = Vector2.left;
     public bool canDash;
@@ -12,6 +13,8 @@ public class Player : MonoBehaviour
     public GameObject coin;
     public AudioSource audio;
     public AudioClip clip;
+    private bool isMoving = false;
+    public bool hasLanded = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,21 +24,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        var hit = Physics2D.Raycast(transform.position, input, 1f, wallLayer);
-        canDash = hit.collider != null && hit.collider.CompareTag("Wall");
-
         var newInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        if (canDash && newInput != Vector2.zero)
+        if (rb.velocity.magnitude < 0.1f && newInput != Vector2.zero)
         {
             input = newInput;
-            //convert input to z angle
-            var angle = Mathf.Atan2(input.y, input.x) * Mathf.Rad2Deg;
-            transform.eulerAngles = new Vector3(0, 0, angle + 90);
+            transform.up = -input;
         }
 
         rb.velocity = speed * input;
-
     }
     
     void OnTriggerEnter2D(Collider2D collision)
@@ -48,5 +44,6 @@ public class Player : MonoBehaviour
             Destroy(collision.gameObject);
         }
     }
-    
+
+
 }
---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleporter : MonoBehaviour
+{
+    public string sceneName;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
